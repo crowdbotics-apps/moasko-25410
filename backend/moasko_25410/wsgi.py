ec91868fff8a784879556b28bd9ce2432da34ee2
Crowdbotics <team@crowdbotics.com>
2021-04-01 12:54:01 +0000

    Update models

--- backend/moasko_25410/wsgi.py
+++ backend/moasko_25410/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moasko_25410.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moasko_25410.settings")
 
 application = get_wsgi_application()
